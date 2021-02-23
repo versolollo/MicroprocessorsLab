f275a3ef02d63ec6d153f24b975e0d328a8ed19f
Lorenzo <lv518@ic.ac.uk>
2021-02-23 22:49:20 +0000

    double timer ultimated
    Double timer has been completed. You might find some more clever ways of handling the reset of the timer variables.

--- main.s
+++ main.s
@@ -24,7 +24,7 @@ delay:
 	return
 looptwo:
 	movff	0x06,PORTC	; move timer value in PORTC
-	movlw	0x08		; load max value into w
+	movlw	0xFF		; load max value into w
 	cpfslt	0x06		; compare timer value with max value
 	return
 	incf	0x06,F,A	; increase timer value
@@ -34,26 +34,9 @@ loop:
 	movwf	0x06, A
 	movff	0x05,PORTD	; move timer value into PORTD
 	call looptwo
-	movlw	0x02		; load max timer value into W
+	movlw	0xFF		; load max timer value into W
 	cpfslt	0x05, A		
 	return
 	incf	0x05,F,A
 	goto	loop
-	
-
-;start:
-;	movlw 	0x0
-;	movwf	TRISC, A	    ; Port C all outputs
-;	bra 	test
-;loop:
-;	movff 	0x06, PORTC
-;	incf 	0x06, W, A  ; increase file and put result into W
-;test:
-;	movwf	0x06, A	    ; Test for end of loop condition
-;	movlw 	0x63
-;	cpfsgt 	0x06, A
-;	bra 	loop		    ; Not yet finished goto start of loop again
-;	goto 	0x0		    ; Re-run program from start
-;
-;	end	main
 end main
