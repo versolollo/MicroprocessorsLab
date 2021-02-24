4036b7feeeff2202aabf629b7e6338e9aa66e019
Lorenzo <lv518@ic.ac.uk>
2021-02-24 01:01:42 +0000

    WIP-01
    Code is there, but need A LOT of debugging. First test with simulator then with the board.

--- main.s
+++ main.s
@@ -10,22 +10,53 @@ main:
 start:
 	movlw	0x00
 	movwf	TRISD, A	    ; set PORTD to output
+	movwf	TRISF, A	    ; set PORTF to output
 	movlw	0x11		    
 	movwf	PORTD, A	    ; deactivate output enabler for latches
 	movlw	0xFF	
 	movwf	TRISC, A	    ; first bit of PORTC selects latch
 	movwf	TRISB, A	    ; set PORTB to input
 	movwf	TRISE, A	    ; initialise PORTE to input (avoid short circuits)
+loop:
+	BTFSS	PORTC, 0, A	    ; if 0, read
+	call	read
+	BTFSC	PORTC, 0, A	    ; if 1, write
+	call	write
+	goto	loop		    ; repeat loop
 write:
 	movlw	0x00
 	BSF	PORTD, 0, A	    ; disable output enablers
 	BSF	PORTD, 8, A	    
-	movlw	0xFF
 	movwf	TRISE, A	    ; set PORTE to output
 	movf	PORTB, W, A	    ; load value to write in W
 	movwf	PORTE, A	    ; write W into PORTE
 	BTFSS	PORTC, 0, A
-	
+	goto	clock1
+	goto	clock2
+clock1:	BSF	PORTD, 1, A	    ; turn 1 clock on
+	call	delay
+	BCF	PORTD, 1, A	    ; turn 1 clock off
+	goto	endwrite
+clock2:	BSF	PORTD, 9, A	    ; turn 2 clock on
+	call	delay
+	BCF	PORTD, 9, A	    ; turn 2 clock off
+endwrite:
+	movlw	0xFF
+	movwf	TRISE, A	    ; set PORTE back to input
+	return
+read:	
+	movlw	0xFF
+	movwf	PORTE, A	    ; set PORTE to input
+	BTFSS	PORTC, 0, A	    ; if bit is 0
+	BCF	PORTD, 0, A	    ; enable latch number 1
+	BTFSC	PORTC, 0, A	    ; if bit is 1
+	BCF	PORTD, 8, A	    ; enable latch number 0
+	call	delay
+	movf	PORTE, W, A	    ; read value in PORTE
+	movwf	PORTF, A	    ; write value into PORTF
+	BSF	PORTD, 0, A
+	BSF	PORTD, 8, A
+	return
 delay:
 	movlw	0x00
 	movwf	0x06, A		; reset looptwo timer
@@ -37,21 +68,4 @@ timer:
 	return
 	incf	0x06,F,A
 	goto	timer
-	
-
-;start:
-;	movlw 	0x0
-;	movwf	TRISC, A	    ; Port C all outputs
-;	bra 	test
-;loop:
-;	movff 	0x06, PORTC
-;	incf 	0x06, W, A  ; increase file and put result into W
-;test:
-;	movwf	0x06, A	    ; Test for end of loop condition
-;	movlw 	0x63
-;	cpfsgt 	0x06, A
-;	bra 	loop		    ; Not yet finished goto start of loop again
-;	goto 	0x0		    ; Re-run program from start
-;
-;	end	main
 end main
