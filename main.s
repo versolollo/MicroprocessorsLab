7cbfd0c62cf48fe51c7d4e7901343a3664dc38fe
MartinGIgnatov <55231375+MartinGIgnatov@users.noreply.github.com>
2021-03-03 11:40:41 +0200

    Serial to paralel working

--- main.s
+++ main.s
@@ -3,22 +3,35 @@
 psect	code, abs
 	
 main:
-	org	0x0
+	org	0x0 
 	goto	start
 
 	org	0x100		    ; Main code starts here at address 0x100
 start:
-	movlw 	0x0
-	movwf	TRISB, A	    ; Port C all outputs
-	bra 	test
-loop:
-	movff 	0x06, PORTB
-	incf 	0x06, W, A
-test:
-	movwf	0x06, A	    ; Test for end of loop condition
-	movlw 	0x63
-	cpfsgt 	0x06, A
-	bra 	loop		    ; Not yet finished goto start of loop again
-	goto 	0x0		    ; Re-run program from start
+	call 	SPI_MasterInit
+	movlw	0x0f
+	bra	SPI_Master_Transmit
+	
+	
+SPI_MasterInit:
+	bcf	CKE
+	movlw	(SSP2CON1_SSPEN_MASK)|(SSP2CON1_CKP_MASK)|(SSP2CON1_SSPM1_MASK)
+	MOVWF	SSP2CON1, A
+	bcf	TRISD, PORTD_SDO2_POSN, A
+	bcf	TRISD, PORTD_SCK2_POSN, A
+	bcf	TRISD, PORTD_RD0_POSN, A
+	bcf	PORTD, PORTD_RD0_POSN, A
+	bsf	PORTD, PORTD_RD0_POSN, A
+	return
+	
+SPI_Master_Transmit:
+	movwf	SSP2BUF, A
+Wait_Transmit:
+	btfss	SSP2IF
+	bra	Wait_Transmit
+	bcf	SSP2IF
+	return
+	
 
+	
 	end	main
